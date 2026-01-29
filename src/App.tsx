b8dba34623271bc9b2a87d67f9fe8a69c16537df
Monique Carvalho <moniquecarvalho654@gmail.com>
2026-01-29 14:31:43 -0300

    feat: adicionar componente Hero e botão compartilhável

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Header from "./components/shared/Header";
+import { Hero } from './components/Hero'
 import Footer from "./components/shared/Footer"
+
 
 function App() {
   return (
     <>
     <div>
       <Header />
+      
+      <Hero />
       <Footer />
     </div>
     </>
