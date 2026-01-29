d19a7fec92cf49958ea6dfc489b5a332ef0d9cb9
Monique Carvalho <moniquecarvalho654@gmail.com>
2026-01-29 18:53:35 -0300

    fix: corrigir cor da borda no componente Header

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -2,7 +2,7 @@ import logoBarbearia from "../../assets/logo-barbearia.jpg";
 
 const Header = () => {
   return (
-    <header className="flex  justify-between items-center px-6 py-4 fixed top-0 left-0 right-0 bg-[#0A0A0A] backdrop-blur-sm border-b border-[rgba(212,175,50,0.2)] z-50">
+    <header className="flex  justify-between items-center px-6 py-4 fixed top-0 left-0 right-0 bg-[#0A0A0A] backdrop-blur-sm border-b border-[rgba(212,175,55,0.2)] z-50">
       <div className="flex items-center gap-2">
         <img
           src={logoBarbearia}
